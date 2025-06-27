829350874624fd921ece0f888a8e95f7a904492c
a112233-dt <nguyen@hopham.info>
2025-06-27 10:53:24 +0700

    Delete Warp+ OpenWrt - Định tuyến QT directory

--- Warp+ OpenWrt - Định tuyến QT/setup_warpplus_with_ipv4vn_fetch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# setup_warpplus_with_ipv4vn_fetch.sh nội dung như đã cung cấp ở trên
-# (Rút gọn để tránh lặp lại toàn bộ trong khối mã)
-echo "Cài đặt Warp+ và tải danh sách IPv4 VN..."
--- Warp+ OpenWrt - Định tuyến QT/update_ipv4vn.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# update_ipv4vn.sh nội dung như đã cung cấp ở trên
-echo "Cập nhật danh sách IPv4 Việt Nam..."
